435ba8652c8219127561fee088562604f1584c9b
Tony Rogvall <tony@rogvall.se>
2017-11-02 18:40:08 +0100

    load columns are faster right now

--- src/nist.erl
+++ src/nist.erl
@@ -69,8 +69,8 @@ read_image(F, I) when I >= 0 ->
     file:read(F#nist_image_file.fd, N).
 
 %% image as column vector (coded as matrix)
-%% image_to_vector(Bin) when is_binary(Bin) ->
-%%    matrix:from_list([[ X/255] || <<X>> <= Bin ],float32).
+image_to_vector(Bin) when is_binary(Bin) ->
+    matrix:from_list([[ X/255] || <<X>> <= Bin ],float32).
 %%    matrix:transpose(matrix:from_list([[X/255 || <<X>> <= Bin ]],float32)).
 
 %% return the image as a column vector
